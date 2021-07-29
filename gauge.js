45b2fe698deb704af87b9ed9c79e7930f6b95914
Orenda Inc <66438618+orenda-group@users.noreply.github.com>
2021-07-29 18:52:38 -0400

    Create gauge.js

---
+++ gauge.js
@@ -0,0 +1,30 @@
+looker.plugins.visualizations.add({
+	create: function(element, config){
+		element.innerHTML = "<h1>Ready to render!</h1>";
+	},
+	updateAsync: function(data, element, config, queryResponse, details, doneRendering){
+		var html = "";
+		for(var row of data) {
+			var cell = row[queryResponse.fields.dimensions[0].name];
+			html += LookerCharts.Utils.htmlForCell(cell);
+		}
+    
+    var data = [
+        {
+          domain: { x: [0, 1], y: [0, 1] },
+          value: 450,
+          title: { text: "Speed" },
+          type: "indicator",
+          mode: "gauge+number",
+          delta: { reference: 400 },
+          gauge: { axis: { range: [null, 500] } }
+        }
+      ];
+		console.log(data)
+		var layout = { width: 600, height: 400 };
+		element.innerHTML = "<div id='tester' style='width:600px;height:250px;'></div>";
+    Plotly.newPlot('tester', data, layout);
+
+    doneRendering()
+	}
+});
